4ebd37774332fb4dcdace6bcb03b994afe66d863
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-16 10:33:53 -0300

    01:15 - Formatando número e moeda

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const n1 = (123).toString();
+const n1 = 1_530.50;
+// 1.530,50
 
-console.log(typeof n1);
+// pega pelo sistema operacional
+// const resultado = n1.toLocaleString();
+
+const resultado = n1.toLocaleString('en-US', { style: 'currency', currency: 'USD', currencyDisplay: 'name' });
+console.log(resultado);
