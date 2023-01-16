0de02cd0fe0131b3f3f1a4f49fe2b2674548afc6
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-16 10:29:25 -0300

    01:13 - Método para trabalhar com potenciação

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-// const resultado = +10.12346.toFixed(3);
-// 10.12
+const resultado = Math.pow(2, 4);
+console.log(resultado);
 
-// console.log(resultado);
-
-const valorCombustivel = 7.283;
-
-const resultado = valorCombustivel * 100;
-
-const valorSemana = (valorCombustivel * 100) * 52;
-console.log(valorSemana.toFixed(2));
+// Objeto Math javascript
