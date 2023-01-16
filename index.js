7bfa32c5d2c9b9f0059550c845563a9f83ee13bb
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-16 09:39:05 -0300

    01:02 - Números inteiros

---
+++ index.js
@@ -0,0 +1,12 @@
+// Número inteiros
+// -10 -9 -8 -7... 0 +1 +2 +3...
+
+// console.log(10 + 5);
+
+// 2 ** 53
+
+// console.log(2 ** 53);
+// 9 007 199 253 740 992 --> 9 quadrilhão
+// console.log(Number.MAX_SAFE_INTEGER);
+
+console.log(0777);
